788ecf4a772b4b0d4a2453ea4e902fd8e0c5af8c
Emma Qian <qianhanmin99@gmail.com>
2019-02-25 23:43:01 -0800

    attempt2

--- svd_off_the_shelf.py
+++ svd_off_the_shelf.py
@@ -1,23 +1,21 @@
 import pandas as pd
 import numpy as np
-from surprise import NormalPredictor
-from surprise import Dataset
-from surprise import Reader
+from surprise import NormalPredictor, Dataset, SVD, Reader
 from surprise.model_selection import cross_validate
 
 df_train = pd.read_csv("data/train.txt", delimiter="\t", encoding="latin_1",
 header = None)
 rated_movies = df_train.iloc[:,1].unique()
-train_data = load_from_df(df_train, Reader())
-
+train_data = Dataset.load_from_df(df_train, Reader())
+train_data = train_data.build_full_trainset()
 df_test = pd.read_csv("data/test.txt", delimiter="\t", encoding="latin_1",
 header = None)
-print(df_test.shape)
-df_test = df[~df.iloc[:,1].isin(rated_movies)]
-print(df_test.shape)
-train_data = load_from_df(df_test, Reader())
+df_test = df_test[df_test.iloc[:,1].isin(rated_movies)]
+train_data = Dataset.load_from_df(df_test, Reader())
 
 algo = SVD()
 algo.fit(train_data)
-
-print()
+U = algo.pu
+V = algo.qi
+print(U)
+print(V)
